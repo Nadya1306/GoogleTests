3254f35ed40bf737485dc376de67ca415d5195eb
Serge Menshikh <sergemenshikh@gmail.com>
2017-04-23 17:20:32 +0100

    Second commit

---
+++ src/GmailApplication/GmailPageObjcts/LearnMorePage.java
@@ -0,0 +1,15 @@
+package GmailApplication.GmailPageObjcts;
+
+import org.openqa.selenium.WebDriver;
+
+
+public class LearnMorePage {
+    private WebDriver driver ;
+
+    public LearnMorePage( WebDriver driver){
+
+        this.driver = driver;
+    }
+
+
+}
